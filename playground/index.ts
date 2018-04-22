f3c4ecaced757d0c33ba4982edbcbd271399a1fb
goekaypamuk <goekaypamuk@gmail.com>
2018-04-22 19:32:29 +0200

    Init Setup

--- playground/index.ts
+++ playground/index.ts
@@ -6,7 +6,7 @@ import { NgModule } from '@angular/core';
 import { Component } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { SampleModule }  from '@angular Qlik/angular-qlik-api';
+import { SampleModule }  from 'angular-qlik-api';
 
 @Component({
   selector: 'app',
--- playground/systemjs.config.js
+++ playground/systemjs.config.js
@@ -27,7 +27,7 @@
       // other libraries
       rxjs: 'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
-      '@angular Qlik/angular-qlik-api': '../dist'
+      'angular-qlik-api': '../dist'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -45,7 +45,7 @@
       rxjs: {
         defaultExtension: 'js'
       },
-      '@angular Qlik/angular-qlik-api': {
+      'angular-qlik-api': {
         main: 'angular-qlik-api.umd.js',
         defaultExtension: 'js'
       }
